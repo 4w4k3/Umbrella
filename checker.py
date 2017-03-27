aa50d7e7aaef8175625d8fb1bdd5999d85666ab3
Alisson Moretto <4w4k3@users.noreply.github.com>
2017-03-27 08:43:36 -0300

    Fixed

--- checker.py
+++ checker.py
@@ -2,60 +2,67 @@
 # Written by: * Alisson Moretto - 4w4k3
 # https://github.com/4w4k3/Umbrella
 # Licensed under the BSD-3-Clause
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
+import sys
+import platform
+
+os.system('clear')
+print '''\n ### PLEASE DON'T INTERRUPT THE INSTALLATION OF DEPENDENCIES ###\n'''
 
 def banner(text, char="*"):
     print(char * len(text) + "****")
     print(char + " " + text + " " + char)
     print(char * len(text) + "****")
 
+checker = platform.architecture()
+if checker[0] == "64bit":
+    if os.path.exists('.ARCHADDED'):
+        print "OK"
+    else:
+        print "You're running 64bit System, its necessary install i386 arch."
+        a = raw_input("Do you wanna continue? (y/n) [This can take a few moments]: ")
+        if a.upper() == "Y":
+            os.system('sudo dpkg --add-architecture i386')
+            os.system('sudo apt-get dist-upgrade -yy && apt-get upgrade -yy')
+            os.system('sudo apt-get -f install')
+            os.system('mkdir .ARCHADDED')
+        else:
+            sys.exit(0)
+else:
+    print "Arch i386 FOUND."
 
-def install_dependencies():
-    """ Install the dependencies needed to run the program """
-    os.system('apt-get install sudo')
-    os.system('sudo apt-get install wget -y')
-    os.system('sudo dpkg --add-architecture i386')
-    os.system('sudo apt-get update && sudo apt-get install wine -y')
-    os.system('sudo apt-get dist-upgrade -yy && apt-get upgrade -yy')
-    os.system('sudo apt-get install wine32 -y')
-    os.system('sudo apt-get -f install')
-    os.system('sudo apt-get install wine32 -y')
+banner(" ~ SEARCHING FOR WGET")
+abc = os.system('which wget')
+if len(str(abc)) == 1:
+    print "[*] WGET FOUND"
+else:
+    print "[!] WGET NOT FOUND, INSTALLING IT"
+    os.system("sudo apt-get install wget -y")
+
+banner(" ~ SEARCHING FOR WINE")
+abc = os.system('which wine')
+if len(str(abc)) == 1:
+    print "[*] WINE FOUND"
+else:
+    print "[!] WINE NOT FOUND, INSTALLING IT"
+    os.system("sudo apt-get install wine -y")
+    os.system("sudo apt-get install wine32 -y")
     os.system('sudo apt-get install winbind -y')
-    os.system('clear')
-    banner("Press enter to default Winecfg to Windows 7")
-    raw_input()
-    os.system('winecfg')
-    os.system('clear')
 
-def download_vc_for_py():
+banner(" ~ SEARCHING FOR PYTHON2.7 ON WINE")
+if os.path.exists('/root/.wine/drive_c/Python27'):
+    print '/root/.wine/drive_c/Python27'
+    print "[*] PYTHON 2.7 FOUND"
+else:
+    print "[!] PYTHON 2.7 NOT FOUND, INSTALLING IT"
     os.system('sudo wine msiexec /i python-2.7.12.msi /L*v log.txt')
-    banner("Downloading VCForPython27.msi, please wait...")
-    os.system('wget https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi')
-    os.system('sudo wine msiexec /i VCForPython27.msi /L*v log2.txt')def download_python():
-    """ Download python to WINDOWS for compile"""
-    os.system('clear')
 
-def download_python_win_exten():
-    """ Download Windows extenstion for python without checking the checksum.. """
-    banner("Installing pywin32-220.win32-py2.7.exe (Windows extension), please wait...")
+banner(" ~ SEARCHING FOR PYWIN32")
+if os.path.isfile("/root/.wine/drive_c/Python27/Removepywin32.exe"):
+    print "/root/.wine/drive_c/Python27/Removepywin32.exe"
+    print "[*] PYWIN32 FOUND"
+else:
+    print "[!] PYWIN32 NOT FOUND, INSTALLING IT"
     os.system('sudo wine pywin32-220.win32-py2.7.exe')
-    os.system('sudo wine /root/.wine/drive_c/Python27/python.exe /root/.wine/drive_c/Python27/Scripts/pip.exe install pyinstaller')
-    os.system('clear')
-    os.system('mkdir .OK')
-    os.system('sudo rm -Rf log.txt')
-
-
-def main():
-    print("\n")
-    banner("Installing dependencies..")
-    print("\n")
-    install_dependencies()
-    download_python()
-    print("\n")
-    banner("Moving to dependent files..")
-    print("\n")
-    download_python_win_exten()
-    
-
-if __name__ == '__main__':
-    main()
